d3efdee4598039b905b81d32c5befb74c350ca4d
Arshad <arshadmohammedgm@gmail.com>
2025-08-10 17:13:40 +0300

    gitaction config

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/health')
+
 def health():
     return '200 ok', 200
 
 @app.route('/data')
+
 def data():
     return jsonify({"message": "Hello from DevOps!"})
 
 if __name__ == '__main__':
+    
     app.run(host='0.0.0.0', port=5000)
