9d0d031f26f351997c6d9a84f65850a0ab36e3bb
Arshad <arshadmohammedgm@gmail.com>
2025-08-11 08:10:38 +0300

    new changes

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, jsonify
 import os
 
+
 app = Flask(__name__)
+
 
 @app.route('/health')
 def health():
     return '200 ok', 200
+
 
 @app.route('/data')
 def data():
     msg = os.getenv("APP_MESSAGE", "Hello from DevOps!")
     env = os.getenv("APP_ENV", "development")
     return jsonify({"message": msg, "env": env})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
